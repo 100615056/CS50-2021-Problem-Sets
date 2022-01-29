0f4bafeba79788eaa6565bd9601dfc717bf43988
Angelica <32560782+100615056@users.noreply.github.com>
2022-01-29 16:00:57 -0500

    Delete test.sql

--- movies/test.sql
+++
@@ -1,4 +0,0 @@
-SELECT movies.title FROM movies
-INNER JOIN stars on movies.id = stars.movie_id
-INNER JOIN people on stars.person_id = people.id
-WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
